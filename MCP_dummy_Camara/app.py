88cfcf47a8450d6a85ec96460091310f8c0e44cb
kostas chartsias <kchartsias@yahoo.gr>
2025-10-19 11:16:25 +0300

    minor fixes

--- MCP_dummy_Camara/app.py
+++ MCP_dummy_Camara/app.py
@@ -1,12 +1,8 @@
 import logging
 from mcp.server.fastmcp import FastMCP
-<<<<<<< Updated upstream:MCP_dummy_Camara/app.py
 from MCP_dummy_Camara.tools.qod import create_qod_session, get_qod_session, delete_qod_session
-=======
-from MCPserver_2.tools.qod import create_qod_session, get_qod_session, delete_qod_session
 from dotenv import load_dotenv
 import os
->>>>>>> Stashed changes:MCPserver_2/app.py
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s")
 logger = logging.getLogger("MCP server")
