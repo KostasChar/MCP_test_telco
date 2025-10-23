b8e6f06b58d726c3c382281bc65242f604affcca
chkopa <chkopa@intracom-telecom.com>
2025-10-23 10:11:58 +0300

    minor codebase fixes

--- Camara_Backend/app.py
+++ Camara_Backend/app.py
@@ -1,11 +1,21 @@
+import os
 import connexion
+from dotenv import load_dotenv
+
+
+load_dotenv()
+# Load environment variables with defaults
+
+HOST = os.getenv("API_HOST", "127.0.0.1")
+PORT = int(os.getenv("API_PORT", 8081))
 
 # Create Connexion app
 app = connexion.App(__name__, specification_dir="./")
 app.add_api("openapi.yaml", strict_validation=True)
 
 if __name__ == "__main__":
-    print("Starting  API server...")
-    print("Swagger UI available at: http://localhost:8080/ui/")
- #   print("OpenAPI JSON available at: http://localhost:8080/openapi.json")
-    app.run(host="127.0.0.1", port=8080)
+    print("Starting API server...")
+    print(f"Swagger UI available at: http://{HOST}:{PORT}/ui/")
+    # print(f"OpenAPI JSON available at: http://{HOST}:{PORT}/openapi.json")
+    app.run(host=HOST, port=PORT)
+
